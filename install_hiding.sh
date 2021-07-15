6454d6926517d641468ca2b563dd5842af2a6091
Vincent Fontaine <ftechhelp@gmail.com>
2021-07-14 19:51:07 -0700

    Added comment for install

--- install_hiding.sh
+++ install_hiding.sh
@@ -4,3 +4,4 @@
 #-Install dependencies (nordvpn, net-tools, macchanger, cron) [  ]
 #-Add commands dynamically to ~/.bashrc and refresh it (. ~/.bashrc) [  ]
 #-Copy hiding.sh to specified path with parameter or default to ~ [  ]
+#-Copy cronjob to /etc/cron.d/hiding [  ]
