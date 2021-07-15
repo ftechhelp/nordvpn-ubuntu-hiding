04b2e3530650296ebab42e288ee3d7fbe3240b02
Vincent Fontaine <ftechhelp@gmail.com>
2021-07-15 10:28:35 -0700

    install dependencies

--- install_hiding.sh
+++ install_hiding.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 #TODO (Install Script)
-#-Install dependencies (nordvpn, net-tools, macchanger, cron) [  ]
+#-Install dependencies (nordvpn, net-tools, macchanger, cron) [ X ]
 #-Add commands dynamically to ~/.bashrc and refresh it (. ~/.bashrc) [  ]
 #-Copy hiding.sh to specified path with parameter or default to ~ [  ]
 #-Copy cronjob to /etc/cron.d/hiding [  ]
+
+#Install dependencies
+
+#NordVPN
+sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh)
+sudo nordvpn login
+#net-tools
+sudo apt-get install net-tools
+#macchanger
+sudo apt-get install macchanger
+#cron
+sudo apt-get install cron
