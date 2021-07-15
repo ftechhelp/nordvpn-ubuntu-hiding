3c44a359d13a3a868b2c4f73267659afc13a7fd6
Vincent Fontaine <ftechhelp@gmail.com>
2021-07-15 11:55:08 -0700

    Copy hiding.sh to specified path with parameter or default to /tmp/SystemScripts

--- install_hiding.sh
+++ install_hiding.sh
@@ -3,11 +3,11 @@
 #TODO (Install Script)
 #-Install dependencies (nordvpn, net-tools, macchanger, cron) [ X ]
 #-Add commands dynamically to ~/.bashrc and refresh it (. ~/.bashrc) [  ]
-#-Copy hiding.sh to specified path with parameter or default to ~ [  ]
+#-Copy hiding.sh to specified path with parameter or default to /tmp/SystemScripts [ X ]
 #-Copy cronjob to /etc/cron.d/hiding [  ]
 
 #Install dependencies
-
+<<COMMENT
 #NordVPN
 sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh)
 sudo nordvpn login
@@ -17,3 +17,10 @@ sudo apt-get install net-tools
 sudo apt-get install macchanger
 #cron
 sudo apt-get install cron
+COMMENT
+#Install hide and unhide commands on local enviroment
+install_path=${1:-"/tmp/SystemScripts"}
+sudo mkdir /tmp/SystemScripts
+sudo chmod 777 /tmp/SystemScripts
+sudo cp hiding.sh $install_path
+sudo chmod 777 $install_path
